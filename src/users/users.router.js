5ba28ebe089c58b891fb59980938b995ffd74228
Luis N. Martinez <boue123@gmail.com>
2022-10-17 21:20:26 -0500

    Skeleton finished

--- src/users/users.router.js
+++ src/users/users.router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const userServices = require('./users.services');
+const adminValidate = require('../middlewares/role.middleware');
 //? esta combinacion espara manejar rutas protegidas
 const passport = require('passport');
 require('../middlewares/auth.middleware')(passport);
@@ -16,17 +17,27 @@ router.route('/me')
   .get(
     passport.authenticate('jwt', {session: false}),
     userServices.getMyUser)
-  //.patch()
-  //.delete() 
-  
+  .patch(
+    passport.authenticate('jwt',{session: false}),
+    userServices.patchMyUser)
+  .delete(
+    passport.authenticate('jwt', {session: false}),
+    userServices.deleteMyUser) 
+
 
 
 //? /api/v1/users/:id
 //? dynamics router by ID when one route has differents requests /verbs
 router.route('/:id')
   .get(userServices.getUserById)
-  .patch(userServices.patchUser)
-  .delete(userServices.deleteUser)
+  .patch(
+    passport.authenticate('jwt', {session: false}),
+    adminValidate,
+    userServices.patchUser)
+  .delete(
+    passport.authenticate('jwt', {session: false}),
+    adminValidate,
+    userServices.deleteUser)
 
 
 
